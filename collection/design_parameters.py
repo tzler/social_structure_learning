762bafeaf4ef346453473f8adb9b8cf38d453fa8
tyler <tyler.ray.bonnen@gmail.com>
2017-10-20 18:53:09 -0700

    including data

---
+++ collection/design_parameters.py
@@ -0,0 +1,39 @@
+"""Collect design parameters."""
+
+from psychopy import visual
+import numpy as np
+
+
+def intervals():
+    """Set shock timing, stimulus length, waiting period before experiment."""
+    shock_lag = 3
+    stimulus_length = 4
+    n_second_wait = 2
+    return shock_lag, stimulus_length, n_second_wait
+
+
+def design():
+    """Define experimental structure that matches stimuli in video."""
+    isi = [9,5,7,5,7,7,5,7,7,5,5,5,5,7,5,7,5,5,7,5,7,5,7,5,7,5,7,5,5,7,7,5,7,7,5,7,5,7,7,5,5,7,7,5,7]
+    CS  = [1,0,0,1,0,1,0,1,0,1,1,0,1,0,1,0,0,1,0,1,1,0,1,0,0,1,0,1,1,0,1,0,0,1,1,0,1,0,1,0,0,1,0,0,1]
+    US  = [1,0,0,0,0,0,0,0,0,1,0,0,1,0,1,0,0,0,0,1,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    # temporary conversion for inputing CS vector into biopac.CS(CS) swap 0s and 1s
+    CS = np.abs(np.array(CS)-1)
+    return isi, CS, US
+
+
+def indices(isi, stimulus_length):
+    # set indices for triggering biopac
+    US_onset = - 1  # junk number -- any number not in timecourse
+    stim_i = 0
+    CS_onset = isi[stim_i]
+    CS_offset = CS_onset + stimulus_length
+    return US_onset, CS_onset, CS_offset, stim_i
+
+
+def video_dimensions(video_path, fullscreen=0, x_size=1000, y_size=1000):
+    """load movie onto background."""
+    window = visual.Window([x_size, y_size], fullscr=fullscreen, units="pix", color=[1, 1, 1])
+    movie = visual.MovieStim3(window, video_path, flipVert=False)
+    window.mouseVisible = False
+    return movie, window
---
+++ collection/experiment_main.py
@@ -0,0 +1,55 @@
+"""Call modules for three main segments of the experiment on day 1."""
+
+import name_subject
+import instructions
+import stimuli
+import exit_questions
+
+# TO DO: fix this warning: Class SDLTranslatorResponder is implemented in both /Users/ssnl_booth2/anaconda/envs/experimental/lib/python2.7/site-packages/pygame/.dylibs/libSDL-1.2.0.dylib and /Library/Frameworks/SDL.framework/Versions/A/SDL. One of the two will be used. Which one is undefined.
+# TO DO: fix this warning: User requested fullscreen with size [800 600], but screen is actually [1920, 1080]. Using actual size
+# TO DO: fix this warning: pyo audio lib was requested but not loaded: ImportError('No module named pyo',)
+# TO DO: figure out day two: video recorder to see if they check their hands
+# TO DO: figure out day two:
+# TO DO: figure out day two:
+# TO DO: figure out day two:
+
+subject_id = name_subject.new()
+
+
+def run_subject(subject_id):
+    """Run experiment for one subject."""
+    #
+
+    self_report, window = instructions.run()
+
+    """
+    'instructions' presents slides stored in instructions/; subjects
+    self administer shock at a level they decide and provide self-report of
+    their experience, passed on in 'self_report'
+    """
+
+    # TO DO: fix bug with stimulus markers being sent to eyelink
+    # TO DO: check biopac-gaze-video alignment across entire experiment
+    # TO DO: make sure the laggy video is just processing speed
+    # TO DO: update stimuli markers to reflect ACTUAL design
+    self_report, window = stimuli.run(self_report, window, subject_id)
+
+    """
+    'video_module' presents stimuli of model undergoing fear conditioning and
+    extinction; sends stimulus markers for US and CS to biopac and eyelink to
+    align video + condition markers with gaze and physio data; records skin
+    conductance and saves in neuroview, eyelink data saved in gaze_data/.
+    """
+
+    # TO DO: fix bug with color question -- it allows spaces as answers
+    # TO DO: make sure we dont override previous subjects data
+    exit_questions.run(window, self_report, subject_id)
+
+    """
+    'exit_questions' are taken from slides in 'instruction/'; results saved in
+    'self_report_data/' along with self_report passed from 'instructions'.
+    """
+
+
+if __name__ == "__main__":
+    run_subject(subject_id)
